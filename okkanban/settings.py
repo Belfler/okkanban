8d8156433053c90fc24a802a9deef63dc72a57d7
Alexander Khoroshavin <alexander@khoroshav.in>
2021-11-03 23:34:26 +0300

    Add media settings

--- okkanban/settings.py
+++ okkanban/settings.py
@@ -97,6 +97,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 AUTH_USER_MODEL = 'core.CustomUser'
 
 LOGIN_URL = 'core:login'
